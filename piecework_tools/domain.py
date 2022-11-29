b5609272b4b9144e6ceec358333646a8035bf7e1
me <tkutlesa@gmail.com>
2022-11-29 07:04:15 -0500

    ini domain module, added activity enum

---
+++ piecework_tools/domain.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+
+class Activity(Enum):
+    BOARD = "board"
+    BREAK = "break"
+    TRAVEL = "travel"
+    PACK = "pack"
+    TRANSFER = "transfer"
+    WASHROOM = "washroom"
+    TOOLS = "tools"
+    ORGANIZATION = "organization"
+    MATERIAL = "material"
