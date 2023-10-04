644e7f2df4d2577c9faad6a2603067a3cfe90347
me <tkutlesa@gmail.com>
2023-10-04 17:31:25 -0400

    no time zone, use tz true

--- piecework_tools/settings.py
+++ piecework_tools/settings.py
@@ -124,11 +124,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "America/Toronto"
+# TIME_ZONE = "America/Toronto"
 
 USE_I18N = True
 
-USE_TZ = False
+USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
