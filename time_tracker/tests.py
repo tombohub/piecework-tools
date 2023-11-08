666f0a95507487ca9c470d5a0bcf3b5b30205c26
me <tkutlesa@gmail.com>
2023-11-07 23:12:28 -0500

    bez

--- time_tracker/tests.py
+++ time_tracker/tests.py
@@ -5,10 +5,11 @@ from .models import ActivityTime
 
 # Create your tests here.
 class GeneralTestCase(TestCase):
-    def setUp(self):
-        ActivityTime.objects.create(
-            start="2021-06-01 00:00:00", end="2021-06-01 00:00:00"
-        )
+    def test_kokoloko(self):
+        assert True
 
-    def test_total_duration_today(self):
-        self.assertEqual(db.total_duration_today(), 0)
+    def test_fail(self):
+        """
+        Test if its False
+        """
+        assert False
